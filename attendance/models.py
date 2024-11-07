0ed14bd691ac269018d7f92378e79d43777c4adc
Dan-megabyte <s@s.s>
2024-11-08 00:45:25 +0100

    add student, club and school models

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField("Date Added")
+
+class Club(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=200)
+
+class Student(models.Model):
+    clubs = models.ManyToManyField(Club)
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=200)
