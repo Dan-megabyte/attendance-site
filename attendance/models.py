88dfcb01cbd2051dcef1cf12f39ec0f85788d9a3
Dan-megabyte <s@s.s>
2024-11-08 01:10:59 +0100

    add string representations

--- attendance/models.py
+++ attendance/models.py
@@ -4,13 +4,19 @@ from django.db import models
 class School(models.Model):
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField("Date Added")
+    def __str__(self) -> str:
+        return self.name
 
 class Club(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     icon = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.name
 
 class Student(models.Model):
     clubs = models.ManyToManyField(Club)
     name = models.CharField(max_length=200)
     icon = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.name
