710891477c01d2a7b8a9fe9c30f17577d016f6f9
Dan-megabyte <s@s.s>
2024-11-08 01:45:33 +0100

    register models

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import School, Club, Student
+
 # Register your models here.
+
+admin.site.register(School)
+admin.site.register(Club)
+admin.site.register(Student)
