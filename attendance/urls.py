ed2aace4ab56020a4030e87ff3a4f8976a6fb61b
Dan-megabyte <s@s.s>
2024-11-09 23:52:57 +0100

    add new_user view

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/new-user", views.new_user, name="new-user"),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseForbidden, HttpRequest
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@require_http_methods(["GET", "POST"])
 def index(request):
     return HttpResponse("Hello, world. You're at the attendance index.")
+
+@require_http_methods(["POST"])
+@login_required
+def new_user(request:HttpRequest):
+    if not request.user.groups:
+        return HttpResponseForbidden("You don't have perms")
+    request.user.user_permissions
+    request.POST
+    User.objects.create_user(username, "lennon@thebeatles.com", "johnpassword")
